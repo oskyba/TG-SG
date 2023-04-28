5ffed6822fb194b16ad640f05da72e3fe2700e4d
afanit0 <mh.romero.88@gmail.com>
2023-04-28 16:24:17 -0300

    Funcionalidad restante

--- sg_frontend_cli/html/cobradores.js
+++ sg_frontend_cli/html/cobradores.js
@@ -35,27 +35,124 @@ function doSearch()
 
     function cargarCobradores() 
     { 
-            fetch('https://ejemplo.com/api/cobradores')
-              .then(response => response.json())
-              .then(data => {
-                const tableBody = document.querySelector('#cobradores-table tbody');
-      
-                data.forEach(cobrador => {
-                  const row = document.createElement('tr');
-                  row.innerHTML = `
-                    <td>${cobrador.id_factura}</td>
-                    <td>${cobrador.id_cliente}</td>
-                    <td>${cobrador.direccion}</td>
-                    <td>${cobrador.fecha}</td>
-                    <td>${cobrador.numero_factura}</td>
-                    <td>${cobrador.importe}</td>
-                    <td>${cobrador.estado}</td>
-                    <td>${cobrador.comentarios}</td>
-                    <td>Lista de Acciones</td>
-                    <td><input type="checkbox" ${cobrador.cobrado ? 'checked' : ''}></td>
-                  `;
-                  tableBody.appendChild(row);
+        fetch('https://644bd91a4bdbc0cc3a9c3baa.mockapi.io/contactabilidad')
+          .then(response => response.json())
+          .then(data => {
+            const tabla = document.getElementById('cobradores-table');
+            const tbody = tabla.querySelector('tbody');
+            data.forEach(cobrador => {
+              const row = document.createElement('tr');
+
+                 /* row.innerHTML = `
+                    <td><div>${cobrador.idFactura}</div></td>
+                    <td><div>${cobrador.idCliente}</div></td>
+                    <td><div>${cobrador.direccion}</div></td>
+                    <td><div>${cobrador.fechaCobro}</div></td>
+                    <td><div>${cobrador.numeroFactura}</div></td>
+                    <td><div>${cobrador.importe}</div></td>
+                    <td><div>${cobrador.estado}</div></td>
+                    <td><div contenteditable="true" maxlength="50">${cobrador.comentario}</div></td>
+                    <td>
+                        <button class="btn btn-secondary btn-sm" onclick="modidificarComentario(this)">Guardar</button>
+                        <button class="btn btn-secondary btn-sm" onclick="cobrarFactura(this)">Cobrar</button>
+                    </td>
+                  `; */
+                   row.innerHTML = `
+                    <td><div>1</div></td>
+                    <td><div>1</div></td>
+                    <td><div>Calle Falsa 123</div></td>
+                    <td><div>28/04/2023</div></td>
+                    <td><div>3</div></td>
+                    <td><div>1000</div></td>
+                    <td><div>Contactado</div></td>
+                    <td><div contenteditable="true" maxlength="50">No se</div></td>
+                    <td>
+                    <button class="btn btn-secondary btn-sm" onclick="modidificarComentario(this)">Guardar</button>
+                    <button class="btn btn-secondary btn-sm" onclick="cobrarFactura(this)">Cobrar</button>
+                    </td>
+                    `; 
+                    tbody.appendChild(row);
+
+                    $("div[contenteditable='true'][maxlength]").on('keyup paste', function (event) {
+                        var cntMaxLength = parseInt($(this).attr('maxlength'));
+                        if ($(this).text().length >= cntMaxLength && event.keyCode != 8 && 
+                                                     event.keyCode != 37 && event.keyCode != 38 && event.keyCode != 39 && 
+                                                     event.keyCode != 40) {
+                          event.preventDefault();
+                          $(this).html(function(i, currentHtml) {
+                             return currentHtml.substring(0, cntMaxLength-1);
+                          });
+                        }
+                        });
                 });
               })
               .catch(error => console.error(error));
+    }
+
+    function cobrarFactura(boton) {
+
+        var preFila = boton.parentNode.parentNode;
+        var posicion = Array.prototype.indexOf.call(preFila.parentNode.children, preFila);
+
+        const fila  = document.querySelectorAll('#cobradores-table tbody tr')[posicion];
+        const id = fila.querySelectorAll('td')[0].textContent;
+
+        fetch(`https://644bd91a4bdbc0cc3a9c3baa.mockapi.io/facturas/${id}`, {
+         method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          estado: "Cobrado"
+        })
+    })
+    .then(response => {
+        cargarCobradores();   
+        if (!response.ok) throw Error(response.status);
+    })
+    .catch(error => {
+        cargarFeedbackError(); 
+      });
+
+    }
+
+    function modidificarComentario(boton) {
+
+        var preFila = boton.parentNode.parentNode;
+        var posicion = Array.prototype.indexOf.call(preFila.parentNode.children, preFila);
+
+        const fila  = document.querySelectorAll('#cobradores-table tbody tr')[posicion];
+        const id = fila.querySelectorAll('td')[0].textContent;
+        const comentario = fila.querySelectorAll('td')[7].textContent;
+
+        fetch(`https://644bd91a4bdbc0cc3a9c3baa.mockapi.io/facturas/${id}`, {
+         method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          comentario: comentario
+        })
+    })
+    .then(response => {
+        cargarCobradores();   
+        if (!response.ok) throw Error(response.status);
+        cargarFeedbackOK();
+    })
+    .catch(error => {
+        cargarFeedbackError(); 
+      });
+
+    }
+
+    function cargarFeedbackOK() {
+        var x = document.getElementById("snackbar");
+        x.className = "show";
+        setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    }
+
+    function cargarFeedbackError() {
+        var x = document.getElementById("snackbarError");
+        x.className = "show";
+        setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
     }
--- sg_frontend_cli/html/facturas.js
+++ sg_frontend_cli/html/facturas.js
@@ -1,3 +1,185 @@
+function limpiarTabla() {
+    const tbody = document.querySelector("tbody");
+    tbody.innerHTML = "";
+}
+
+function cargarFacturas() 
+{ 
+        limpiarTabla();
+        fetch('https://644bd91a4bdbc0cc3a9c3baa.mockapi.io/facturas')
+          .then(response => response.json())
+          .then(data => {
+            const tabla = document.getElementById('facturas-table');
+            const tbody = tabla.querySelector('tbody');
+            data.forEach(facturas => {
+              const row = document.createElement('tr');
+              row.innerHTML = `
+                    <td><div>${facturas.id}</div></td>
+                    <td><div>${facturas.idCliente}</div></td>
+                    <td>
+                        <input name="date" class="datepicker-input" type="hidden" />
+                        <div class="date" contenteditable="true" maxlength="10">${facturas.fechaEmision}</div>
+                    </td>
+                    <td><div contenteditable="true" maxlength="15">${facturas.numeroFactura}</div></td>
+                    <td><div contenteditable="true" maxlength="30">${facturas.importe}</div></td>
+                    <td><div contenteditable="true" maxlength="30">${facturas.estado}</div></td>
+                    <td>
+                        <input name="date" class="datepicker-input" type="hidden" />
+                        <div id="date" class="date" contenteditable="true" maxlength="10">${facturas.fechaVencimiento}</div>
+                    </td>
+                    <td>
+                        <button class="btn btn-secondary btn-sm" onclick="modificarFactura(this)">Guardar</button>
+                        <button class="btn btn-secondary btn-sm" onclick="eliminarFactura(this)">Eliminar</button>
+                    </td>  
+              `; 
+              tbody.appendChild(row); 
+
+              $('.datepicker-input').datepicker({
+                closeText: 'Cerrar',
+                currentText: 'Hoy',
+                monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+                             'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+                monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun',
+                                  'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+                dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+                dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mié;', 'Juv', 'Vie', 'Sáb'],
+                dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá'],
+                weekHeader: 'Sm',
+                dateFormat: 'dd/mm/yy',
+                firstDay: 1,
+                forceParse: false,
+                isRTL: false,
+                showMonthAfterYear: false,
+                onClose: function(dateText, inst) {
+                    $(this).parent().find('.date').focus().html(dateText).blur();
+                }
+                });
+    
+                $('.date').click(function() {
+                    $(this).parent().find('.datepicker-input').datepicker("show");
+                });
+
+                $('.botonEliminar').click(function() {
+                    $(this).closest('tr').remove();
+                  });
+
+                $("div[contenteditable='true'][maxlength]").on('keyup paste', function (event) {
+                    var cntMaxLength = parseInt($(this).attr('maxlength'));
+                    if ($(this).text().length >= cntMaxLength && event.keyCode != 8 && 
+                                                 event.keyCode != 37 && event.keyCode != 38 && event.keyCode != 39 && 
+                                                 event.keyCode != 40) {
+                      event.preventDefault();
+                      $(this).html(function(i, currentHtml) {
+                         return currentHtml.substring(0, cntMaxLength-1);
+                      });
+                    }
+                });
+
+            });
+          })
+          .catch(error => console.error(error));
+}
+
+function agregarFactura() 
+{
+    const id               = document.getElementById("idFactura").textContent;
+    const idCliente        = document.getElementById('idCliente').textContent;
+    const fechaEmision     = document.getElementById('fechaEmision').textContent;
+    const numeroFactura    = document.getElementById('numeroFactura').textContent;
+    const importe          = document.getElementById('importe').textContent;
+    const estado           = document.getElementById('estado').textContent;
+    const fechaVencimiento = document.getElementById('fechaVencimiento').textContent;
+
+    fetch(`https://644bd91a4bdbc0cc3a9c3baa.mockapi.io/facturas`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+            {
+                id: id,
+                idCliente: idCliente,
+                fechaEmision: fechaEmision,
+                numeroFactura: numeroFactura,
+                importe: importe,
+                estado: estado,
+                fechaVencimiento: fechaVencimiento
+            })
+    })
+    .then(response => {
+        closeModal();
+        cargarFacturas();
+        if (!response.ok) throw Error(response.status);
+        cargarFeedbackOK(); 
+    })
+    .catch(error => {
+        cargarFeedbackError(); 
+      });
+}
+
+function modificarFactura(boton) 
+{
+    var preFila = boton.parentNode.parentNode;
+    var posicion = Array.prototype.indexOf.call(preFila.parentNode.children, preFila);
+
+    const fila  = document.querySelectorAll('#facturas-table tbody tr')[posicion];
+    const id = fila.querySelectorAll('td')[0].textContent;
+
+    const idCliente = fila.querySelectorAll('td')[1].textContent;
+    const fechaEmision = fila.querySelectorAll('td')[2].textContent;
+    const numeroFactura = fila.querySelectorAll('td')[3].textContent;
+    const importe = fila.querySelectorAll('td')[4].textContent; 
+    const estado = fila.querySelectorAll('td')[5].textContent; 
+    const fechaVencimiento = fila.querySelectorAll('td')[6].textContent; 
+
+    fetch(`https://644bd91a4bdbc0cc3a9c3baa.mockapi.io/facturas/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          idCliente: idCliente,
+          fechaEmision: fechaEmision,
+          numeroFactura: numeroFactura,
+          importe: importe,
+          estado: estado,
+          fechaVencimiento: fechaVencimiento
+        })
+    })
+    .then(response => {
+        cargarFacturas();  
+        if (!response.ok) throw Error(response.status);
+        cargarFeedbackOK(); 
+    })
+    .catch(error => {
+        cargarFeedbackError(); 
+      });
+}
+
+function eliminarFactura(boton) 
+{
+    var preFila = boton.parentNode.parentNode;
+    var posicion = Array.prototype.indexOf.call(preFila.parentNode.children, preFila);
+    
+    const fila  = document.querySelectorAll('#facturas-table tbody tr')[posicion];
+    const id = fila.querySelectorAll('td')[0].textContent;
+
+    fetch(`https://644bd91a4bdbc0cc3a9c3baa.mockapi.io/facturas/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+      })
+    .then(response => {
+        cargarFacturas(); 
+        if (!response.ok) throw Error(response.status);
+        cargarFeedbackOK();  
+      })
+    .catch(error => {
+        cargarFeedbackError(); 
+      });
+}
+
 function doSearch()
         {
             const tableReg = document.getElementById('facturas-table');
@@ -31,4 +213,34 @@ function doSearch()
             if (searchText == "") {
                 lastTR.classList.add("hide");
             }
+        }
+
+        function showModal() 
+    {
+        var modal = document.getElementById("modalFactura");
+        modal.style.display = "block";
+    }
+
+        function closeModal()
+        {
+            var modal = document.getElementById("modalFactura");
+            modal.style.display = "none";
+        }
+
+        function cargarFeedbackOK() {
+            var x = document.getElementById("snackbar");
+            x.className = "show";
+            setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+        }
+    
+        function cargarFeedbackError() {
+            var x = document.getElementById("snackbarError");
+            x.className = "show";
+            setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+        }
+    
+        function cargarFeedbackSystem() {
+            var x = document.getElementById("snackbarSystem");
+            x.className = "show";
+            setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
         }
